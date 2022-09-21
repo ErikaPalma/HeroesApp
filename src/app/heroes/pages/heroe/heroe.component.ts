fde1c31d5abaf1d9df36807d5a76587c47159606
Erika <erikapalmasan@gmail.com>
2022-09-21 19:21:56 +0200

    Crear la ruta para edición y detalle de un héroe - Método para el héroe por id

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { switchMap } from 'rxjs/operators';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  heroe!: Heroe;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroeId(id)))
+      .subscribe((heroe) => (this.heroe = heroe));
   }
-
 }
