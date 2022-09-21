10e1d1cfa70b9269cb1077eecf5125460b2d58c4
Erika <erikapalmasan@gmail.com>
2022-09-21 19:52:44 +0200

    Crear variable de entorno en los environments

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroe.interface';
 import { switchMap } from 'rxjs/operators';
 import { HeroesService } from '../../services/heroes.service';
@@ -7,18 +7,38 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [],
+  styles: [
+    `
+      .d-flex {
+        display: flex;
+        justify-content: space-around;
+        gap: 20px;
+        margin-top: 100px;
+      }
+
+      @media (max-width: 425px) {
+        .d-flex {
+          flex-direction: column;
+        }
+      }
+    `,
+  ],
 })
 export class HeroeComponent implements OnInit {
   heroe!: Heroe;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(switchMap(({ id }) => this.heroesService.getHeroeId(id)))
       .subscribe((heroe) => (this.heroe = heroe));
+  }
+
+  volver() {
+    this.router.navigate(['/heroes/listado']);
   }
 }
