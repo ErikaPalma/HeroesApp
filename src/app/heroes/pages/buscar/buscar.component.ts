fb69013bc4515e60d4be65b473e975014ac1d597
Erika <erikapalmasan@gmail.com>
2022-09-27 18:11:21 +0200

    Retroalimentación al usuario si no hay resultados

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -6,12 +6,19 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
-  styles: [],
+  styles: [
+    `
+      .tarjeta {
+        width: 50%;
+        margin: 0 auto;
+      }
+    `,
+  ],
 })
 export class BuscarComponent implements OnInit {
   termino: string;
   heroes: Heroe[];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado!: Heroe | undefined;
 
   constructor(private heroesService: HeroesService) {
     this.termino = '';
@@ -22,15 +29,23 @@ export class BuscarComponent implements OnInit {
 
   buscando() {
     this.heroesService
-      .getSugerencias(this.termino)
+      .getSugerencias(this.termino.trim())
       .subscribe((heroes) => (this.heroes = heroes));
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
     const heroe: Heroe = event.option.value;
-    this.termino = heroe.superhero;
-    this.heroesService
-      .getHeroeId(heroe.id!)
-      .subscribe((heroe) => (this.heroeSeleccionado = heroe));
+
+    if (!heroe) {
+      this.heroeSeleccionado = undefined;
+      console.log('No hay valor');
+
+      return;
+    } else {
+      this.termino = heroe.superhero;
+      this.heroesService
+        .getHeroeId(heroe.id!)
+        .subscribe((heroe) => (this.heroeSeleccionado = heroe));
+    }
   }
 }
